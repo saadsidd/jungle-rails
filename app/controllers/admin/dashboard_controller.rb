99fb2f6a1c773c4f70e388feaf8d723734a7bf21
Saad Siddiq <siddiq.saad@gmail.com>
2022-07-28 08:02:53 -0500

    added stock/categories count in admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,8 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD']
 
   def show
+    @stock_count = Product.sum(:quantity)
+    @categories_count = Category.count(:id)
+    puts @categories_count
   end
 end
