a51bbe31d3a01f0d2d21c8f90a30a8ec223fbcbf
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-02 08:14:40 -0500

    added strip/downcase email before saving

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,8 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    @user.email.strip!.downcase!
+
     if @user.save
       session[:user_id] = @user.id
       redirect_to '/', notice: 'User created!'
