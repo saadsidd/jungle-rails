2a6057b3f1cb0891ebeb7d3296ab5cd3a369b1ab
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-02 11:31:49 -0500

    added authenticate_with_credentials method

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :email, uniqueness: true, presence: true
-  validates :password, confirmation: true
+  validates :password, confirmation: true, length: { minimum: 8 }
   validates :password_confirmation, presence: true
 
   before_validation :fix_email
@@ -15,6 +15,19 @@ class User < ApplicationRecord
       self.email.strip!
       self.email.downcase!
     end
+  end
+
+  def self.authenticate_with_credentials(email, password)
+    email.strip!
+    email.downcase!
+    
+    user = User.find_by_email(email)
+
+    if user && user.authenticate(password)
+      return user
+    end
+
+    return nil
   end
   
 end
