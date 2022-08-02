6ac77c6f56e662ec503b60699b8539bd72ade8f7
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-02 08:15:04 -0500

    added email uniqueness, password matching

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,17 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
-  validates :password, presence: true
+  validates :email, uniqueness: true, presence: true
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
+  before_validation :fix_email
+
+  def fix_email
+    if email.present?
+      self.email.strip!
+      self.email.downcase!
+    end
+  end
   
 end
