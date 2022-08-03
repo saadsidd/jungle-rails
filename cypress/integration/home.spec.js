926624f34572758d3e95d77ae2a6997dd2e1981c
Saad Siddiq <siddiq.saad@gmail.com>
2022-08-03 09:04:00 -0500

    added tests to check for 2 products on homepage

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,14 @@
+describe('jungle tests', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it("Products show on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+});
